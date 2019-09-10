d03406f30cd6261e42b800012eb3ddd36727b418
shownola <swasieko@gmail.com>
2019-09-10 16:56:58 -0700

    add mergesort and elapsed time

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
-import demos
 import random
+import time
+from demos import quicksort, mergesort
 
 
 def create_random_list(size, max_val):
@@ -8,7 +9,16 @@ def create_random_list(size, max_val):
         ran_list.append(random.randint(1,max_val))
     return ran_list
 
-size = int(input("What size list would you like to create? Enter a number "))
+size = int(input("What size list would you like to create? Enter a number: "))
 max = int(input("What is the maximum value of the list you are creating? "))
 
-print(create_random_list(size, max))
+l = create_random_list(size, max)
+tic = time.time()
+quicksort(l)
+toc = time.time()
+print("QuickSort Elapsed time -> ", toc-tic)
+
+tic = time.time()
+mergesort(l)
+toc = time.time()
+print("MergeSort Elapsed time -> ", toc-tic)
